b5659731c1417568a614ff4e729752eaf80fcf9c
Abdul Hannan Zahid <hannanzahid27905@gmail.com>
2026-09-10 23:48:34 +0500

    Adding JS file

---
+++ script.js
@@ -0,0 +1,50 @@
+const searchForm = document.querySelector("#search-form");
+const searchBox = document.querySelector("#search-box");
+const searchResult = document.querySelector("#search-result");
+const showMoreBtn = document.querySelector("#show-more-btn");
+
+const accesKey = "XVrYRdv42dn0vHlRAUKRnyDBAD0Bjg3NGWuCnYuElkg";
+let keyword = "";
+let page = 1;
+async function searchImages() {
+    keyword = searchBox.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accesKey}&per_page=12`;
+    const response = await fetch(url);
+    const data = await response.json();
+
+if(page===1){
+    searchResult.innerHTML="";
+}
+
+
+
+    const results = data.results;
+
+
+    // loops through each image object
+
+    results.map((result) => {
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+        // Put image inside link
+        imageLink.appendChild(image);
+        searchResult.appendChild(imageLink);
+    });
+    showMoreBtn.style.display="block";
+
+}
+searchForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    page = 1;
+    searchImages();
+});
+
+showMoreBtn.addEventListener("click",()=>{
+    page++;
+    searchImages();
+});
+
+
